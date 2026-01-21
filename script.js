ac1ecbac05b59c497ddc9f00f56c81940c5203d4
anasJM <anasjamlaoui@gmail.com>
2026-01-21 17:28:35 +0100

    add, substract, multiply and divide function + operate function

---
+++ script.js
@@ -0,0 +1,43 @@
+function add(a, b) {
+    return a + b;
+}
+
+function substract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+let operator;
+let firstNumber;
+let secondNumber;
+
+function operate(a, str, b) {
+    switch (str) {
+        case "+":
+            return add(a, b);
+            break;
+
+        case "-":
+            return substract(a, b);
+            break;
+
+        case "*":
+            return multiply(a, b);
+            break;
+
+        case "/":
+            return divide(a, b);
+            break;
+    
+        default:
+            console.log("select a valid operator!")
+            break;
+    }
+}
